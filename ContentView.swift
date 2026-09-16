fc5444e70bf7d14173cdb364ea7c846966f72808
AndrewZhundrikov <p075908912@gmail.com>
2026-09-16 10:09:47 -0500

    added task variable and list

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks = ["Do homework, clean your room, make food"]
     var body: some View {
-       
+       List(tasks, id: \.self) { task in
+            Text(task)
+        }
     }
 }
